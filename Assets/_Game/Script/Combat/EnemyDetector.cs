17eae79df797e9200361b55ede4913dad96fe96d
Faiz2979 <faizannabil877@gmail.com>
2025-08-06 06:59:19 +0700

    fixed enemy detector

--- Assets/_Game/Script/Combat/EnemyDetector.cs
+++ Assets/_Game/Script/Combat/EnemyDetector.cs
@@ -1,68 +1,70 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class EnemyDetector : MonoBehaviour
 {
-    [SerializeField] List<GameObject> enemies;
-
     [Header("Detection Settings")]
-    [SerializeField] float detectionRadius = 5f;
-    [SerializeField] float debugSphere = 0.5f;
+    [SerializeField] private float detectionRadius = 5f;
+    [SerializeField] private float debugSphere = 0.5f;
+    [SerializeField] private float detectInterval = 0.1f;
 
-    // Start is called before the first frame update
-    void Start()
+    [Space(10)]
+    [Header("Detected Enemies")]
+    [SerializeField] List<GameObject> enemies = new List<GameObject>();
+
+    private WaitForSeconds wait;
+
+    private void Start()
     {
-
+        wait = new WaitForSeconds(detectInterval);
+        StartCoroutine(DetectEnemiesCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator DetectEnemiesCoroutine()
     {
-        DetectEnemies();
-
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, detectionRadius);
-
-        foreach (GameObject enemy in enemies)
+        while (true)
         {
+            DetectEnemies();
+            yield return wait;
+        }
+    }
+
+    private void DetectEnemies()
+    {
+        enemies.Clear();
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, detectionRadius);
+        foreach (var hit in hits)
+        {
+            Enemy enemy = hit.GetComponent<Enemy>();
             if (enemy != null)
             {
-                float distance = Vector3.Distance(transform.position, enemy.transform.position);
-                if (distance <= detectionRadius)
-                {
-                    Gizmos.color = Color.green;
-                    Gizmos.DrawLine(transform.position, enemy.transform.position);
-                    Gizmos.DrawSphere(enemy.transform.position, debugSphere);
-                }
+                enemies.Add(hit.gameObject);
+                Debug.Log("Detected new enemy: " + hit.name);
             }
         }
     }
 
-
-void DetectEnemies()
-{
-    RaycastHit[] hits = Physics.SphereCastAll(transform.position, detectionRadius, Vector3.up);
-
-    foreach (RaycastHit hit in hits)
+    private void OnDrawGizmos()
     {
-        Enemy enemyComponent = hit.collider.GetComponent<Enemy>();
-        GameObject enemyObj = hit.collider.gameObject;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
 
-        if (enemyComponent != null && !enemies.Contains(enemyObj))
+        foreach (var enemy in enemies)
         {
-            enemies.Add(enemyObj);
-            Debug.Log("Detected new enemy: " + enemyObj.name);
+            if (enemy == null) continue;
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, detectionRadius);
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance <= detectionRadius)
+            {
+                Vector3 enemyPos = new Vector3(enemy.transform.position.x, 0, enemy.transform.position.z);
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, enemyPos);
+                Gizmos.DrawWireSphere(enemyPos, debugSphere);
+            }
         }
     }
-}
-
-
-    
 }
